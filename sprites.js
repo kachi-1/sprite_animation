5e7676c5cc318f92e656dc5b737aa272c3ebb2c3
Kachi <99280287+kachi-1@users.noreply.github.com>
2023-02-07 14:42:19 -0600

    hw2 done

--- sprites.js
+++ sprites.js
@@ -3,39 +3,46 @@ let spriteSheet2;
 let moving = 0;
 let spelunky;
 let green;
+let spawns;
+let purple;
 
 
 function preload() {
     spriteSheet = loadImage("imgs/SpelunkyGuy.png");
     spriteSheet2 = loadImage("imgs/Green.png");
+    spriteSheet3 = loadImage("imgs/purple.png");
 }
 
 function setup() {
     createCanvas(400, 400);
     imageMode(CENTER);
 
-    spelunky = new walkingAnimation(spriteSheet,50,50,200,200);
+    spelunky = new walkingAnimation(spriteSheet,80,80,200,200,9);
+    green = new walkingAnimation(spriteSheet2,80,80,200,300,9);
+    purple = new walkingAnimation(spriteSheet3,80,80,200,100,9);
 }
 
 function draw() {
     background(220);
     spelunky.draw();
+    green.draw();
+    purple.draw();
 }
 
 function keyPressed() {
-    if (keyCode === RIGHT_ARROW || keyCode === LEFT_ARROW) {
-        moving = 0;
-    }
+    spelunky.keyPressed(RIGHT_ARROW, LEFT_ARROW);
+    green.keyPressed(LEFT_ARROW, RIGHT_ARROW);
+    purple.keyPressed(LEFT_ARROW, RIGHT_ARROW);
 }
 
 function keyReleased() {
-    if (keyCode === RIGHT_ARROW || keyCode === LEFT_ARROW) {
-        moving = 1;
-    }
+    spelunky.keyReleased(RIGHT_ARROW, LEFT_ARROW);
+    green.keyReleased(LEFT_ARROW, RIGHT_ARROW);
+    purple.keyReleased(LEFT_ARROW, RIGHT_ARROW);
 }
 
 class walkingAnimation {
-    constructor(spriteSheet, sw, sh, dx, dy) {
+    constructor(spriteSheet, sw, sh, dx, dy, animationLength) {
         this.spriteSheet = spriteSheet;
         this.sw = sw;
         this.sh = sh;
@@ -43,22 +50,43 @@ class walkingAnimation {
         this.dy = dy;
         this.u = 0;
         this.v = 0;
+        this.animationLength = animationLength;
         this.currentFrame = 0;
         this.moving = 0;
         this.xDirection = 1;
     }
 
     draw() {
-        this.u = (this.moving != 0) ? this.currentFrame % this.animationLength: 0;
+        this.u = (this.moving != 0) ? this.currentFrame % this.animationLength : 0;
+        push(); 
         translate(this.dx, this.dy);
         scale(this.xDirection,1);
+       
 
-        image(this.spriteSheet,80,80,this.sw,this.sh,this.u*this.sw,this.v*this.sh);
-
-        if (this.frameCount % 6 == 0) {
+        image(this.spriteSheet,0,0,this.sw,this.sh,this.u*this.sw,this.v*this.sh,this.sw,this.sh);
+        pop();
+        if (frameCount % 6 == 0) {
             this.currentFrame++;
         }
 
-        this.dx = this.moving;
+        this.dx += this.moving;
+    }
+
+    keyPressed(right, left) {
+        if (keyCode == right) {
+            this.moving = 1;
+            this.xDirection = 1;
+            this.currentFrame = 1;
+        }else if (keyCode == left) {
+            this.moving = -1;
+            this.xDirection = -1;
+            this.currentFrame = 1;
+        }
+    }
+
+    keyReleased(right, left) {
+        if (keyCode === right|| keyCode === left) {
+            this.moving = 0;
+        }
     }
 }
